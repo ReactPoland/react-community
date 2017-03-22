ac9762953750840ce3156550618475bfdc9ebf0b
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-22 14:39:36 +0100

    Add missing map TileLayer

--- src/containers/World/World.js
+++ src/containers/World/World.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, Marker, Popup } from 'react-leaflet-universal';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet-universal';
 
 const position = [51.505, -0.09];
 
@@ -12,6 +12,10 @@ export default class World extends Component {
         <h1>World</h1>
         <p>Here will be the map component</p>
         <Map className={styles.map} center={position} zoom={13}>
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
           <Marker position={position}>
             <Popup>
               <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
