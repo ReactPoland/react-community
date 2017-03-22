584dc641a4ae154a97319df37da019df7c7976cf
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-22 14:12:46 +0100

    Add Leaflet map

--- src/containers/World/World.js
+++ src/containers/World/World.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { Map, Marker, Popup } from 'react-leaflet-universal';
+
+const position = [51.505, -0.09];
 
 export default class World extends Component {
   render() {
     const styles = require('./World.scss');
+
     return (
-      <div className={styles.worldPage + ' container'}>
+      <div className={styles.worldPage}>
         <h1>World</h1>
         <p>Here will be the map component</p>
+        <Map className={styles.map} center={position} zoom={13}>
+          <Marker position={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
