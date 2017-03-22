3ea21bcbc003d72083e4d30aa7b7bae63d3f8929
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-22 11:32:00 +0100

    Add Material UI

--- src/client.js
+++ src/client.js
@@ -12,8 +12,13 @@ import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { ReduxAsyncConnect } from 'redux-async-connect';
 import useScroll from 'scroll-behavior/lib/useStandardScroll';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import getRoutes from './routes';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const client = new ApiClient();
 const _browserHistory = useScroll(() => browserHistory)();
