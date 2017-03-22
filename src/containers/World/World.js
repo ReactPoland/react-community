213560520970c1b5be06107065307ddc87ae81fb
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-22 14:52:21 +0100

    Fix map centering issue
    I’m not happy with that solution, but until I understand what’s the
    reason, this is how it will work.

--- src/containers/World/World.js
+++ src/containers/World/World.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet-universal';
 
-const position = [51.505, -0.09];
-
 export default class World extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13,
+      showMap: false
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showMap: true });
+    }, 1000);
+  }
+
   render() {
     const styles = require('./World.scss');
+    const position = [this.state.lat, this.state.lng];
 
     return (
       <div className={styles.worldPage}>
         <h1>World</h1>
         <p>Here will be the map component</p>
-        <Map className={styles.map} center={position} zoom={13}>
-          <TileLayer
-            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={position}>
-            <Popup>
-              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-            </Popup>
-          </Marker>
-        </Map>
+        {
+          this.state.showMap && (
+            <Map className={styles.map} center={position} zoom={this.state.zoom}>
+              <TileLayer
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <Marker position={position}>
+                <Popup>
+                  <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                </Popup>
+              </Marker>
+            </Map>
+          )
+        }
       </div>
     );
   }
