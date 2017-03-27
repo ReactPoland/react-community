99d6a72f65ce9acb7cbf1d90277b12ea64bd14be
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-27 14:02:27 +0200

    Remove unused reducer imports

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-async-connect';
-import { pagination } from 'violet-paginator';
 
 import auth from './auth';
-import counter from './counter';
 import { reducer as form } from 'redux-form';
 import info from './info';
-import widgets from './widgets';
 import mapModule from './mapModule';
 
 export default combineReducers({
@@ -16,13 +12,6 @@ export default combineReducers({
   reduxAsyncConnect,
   auth,
   form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
   info,
-  pagination,
-  widgets,
   map: mapModule
 });
