7325953fbdcdfadb965495f773ab790d1c07c5b9
Robert Kirsz <robert.kirsz@gmail.com>
2017-03-30 15:53:50 +0200

    Move editors to their own folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export ErrorSnackbar from './ErrorSnackbar';
 export Spinner from './Spinner';
 export LoadingScreen from './LoadingScreen';
+export PlainTextEditor from './editors/PlainTextEditor';
+export RichTextEditor from './editors/RichTextEditor';
