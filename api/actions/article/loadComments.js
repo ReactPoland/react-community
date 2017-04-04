09de432f0fab125e640b3f09b05d5fdfc8b70c56
Tolik <talibasya@gmail.com>
2017-04-04 13:20:23 +0200

    load comments doc

--- api/actions/article/loadComments.js
+++ api/actions/article/loadComments.js
@@ -2,6 +2,40 @@ const resp = require('../../utils/serverResp');
 const UserModel = require('../../db').users;
 const ConversationModel = require('../../db').conversations;
 
+/**
+  @api {POST} /api/article/loadComments/ Fetch Comments
+  @apiDescription Retail comments for current article
+  @apiName Fetch Comments
+  @apiGroup Article
+
+  @apiParam {Number} articleId Article id for fetching comments.
+
+  @apiExample Example request:
+  POST /api/article/loadComments/ HTTP/1.1
+
+  {
+      "articleId": 29
+  }
+
+  @apiSuccessExample Example data on success:
+  {
+    "message": [
+      {
+        "id": 7,
+        "body": "reply for reply",
+        ...
+      },
+      {
+        "id": 6,
+        "body": "reply for comment",
+        "conversationId": 1,
+        ...
+      }
+    ],
+    "type": "success"
+  }
+ */
+
 const loadCommentsRequest = async ({ body }) => {
   if (!body) return resp.error('bad request type');
 
