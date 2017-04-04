8aa32a8b816d1e8b553cc4a2b4c9ae6a458d5e34
Tolik <talibasya@gmail.com>
2017-04-04 13:13:37 +0200

    refresh new comment docs

--- api/actions/self/newComment.js
+++ api/actions/self/newComment.js
@@ -10,6 +10,7 @@ const ConversationModel = require('../../db').conversations;
 
   @apiParam {String} body Comment message.
   @apiParam {Number} articleId Article id, which user want to comment.
+  @apiParam {Number} [parentCommentId] Parent comment id. It needs for replies.
 
   @apiPermission Authorized user from the database See how to authorize(#General:Login).
 
@@ -21,11 +22,37 @@ const ConversationModel = require('../../db').conversations;
       "articleId": 27
   }
 
+  @apiExample Example request for reply:
+  POST /api/self/newComment HTTP/1.1
+
+  {
+      "body": "bla bla comment",
+      "articleId": 27,
+      "parentCommentId": 4
+  }
+
+
   @apiSuccessExample Example data on success:
   {
     "message": {
       "id": 3,
       "body": "bla bla comment",
+      "depth": 0
+      "userId": 1,
+      "conversationId": 1,
+      "updatedAt": "2017-04-04T09:31:05.743Z",
+      "createdAt": "2017-04-04T09:31:05.743Z"
+    },
+    "type": "success"
+  }
+
+  @apiSuccessExample Example data on success for reply:
+  {
+    "message": {
+      "id": 3,
+      "body": "bla bla comment",
+      "depth": 1
+      "parentCommentId": 4
       "userId": 1,
       "conversationId": 1,
       "updatedAt": "2017-04-04T09:31:05.743Z",
