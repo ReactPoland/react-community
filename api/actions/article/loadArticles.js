7c4b37f506fb214d01d00e6263f3fedbd9927065
Tolik <talibasya@gmail.com>
2017-04-10 14:29:16 +0200

    handle article/:id request

--- api/actions/article/loadArticles.js
+++ api/actions/article/loadArticles.js
@@ -7,6 +7,24 @@ const loadArticlesRequest = async () => {
     .catch(err => resp.error(err.message));
 };
 
-const loadArticles = () => loadArticlesRequest();
+const loadArticleRequest = async (id) => {
+  return await ArticleModel.findOne({
+    where: {
+      id
+    }
+  }).then(data => {
+    if (!data) throw resp.error('article not found');
+    return resp.success(data);
+  })
+  .catch(err => {
+    throw resp.error(err.message);
+  });
+};
+
+const loadArticles = (req, params) => {
+  if (params.length) return loadArticleRequest(params[0]);
+
+  return loadArticlesRequest();
+};
 
 export default loadArticles;
