7c406b550f56836da50a54f56b40af09e3894da9
Robert Kirsz <robert.kirsz@gmail.com>
2017-04-13 00:29:25 +0200

    Remove unused files

--- src/containers/index.js
+++ src/containers/index.js
@@ -9,13 +9,12 @@ export ArticlesPage from './Articles/ArticlesPage/ArticlesPage';
 export ArticlePage from './Articles/ArticlePage/ArticlePage';
 // OTHER
 export BestPracticesPage from './BestPracticesPage/BestPracticesPage';
-export Chat from './Chat/Chat';
 export Conversation from './Conversation/Conversation';
 export DialogsContainer from './DialogsContainer/DialogsContainer';
 export EventsPage from './EventsPage/EventsPage';
 export HomePage from './HomePage/HomePage';
 export NewArticlePage from './NewArticlePage/NewArticlePage';
-export TutorialsPage from './TutorialsPage/TutorialsPage';
 export ProfilePage from './ProfilePage/ProfilePage';
+export TutorialsPage from './TutorialsPage/TutorialsPage';
 export UsersPage from './UsersPage/UsersPage';
 export WorldPage from './WorldPage/WorldPage';
