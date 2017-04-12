b921df1f43a6e4cfdd6165cc61ccc3182e41f719
Robert Kirsz <robert.kirsz@gmail.com>
2017-04-12 14:43:59 +0200

    Switch to importing PropTypes from “prop-types”

--- src/containers/Articles/ArticlesContainer.js
+++ src/containers/Articles/ArticlesContainer.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // STORE
 import { loadArticles } from 'redux/modules/articlesModule';
--- src/containers/Articles/ArticlesPage/ArticlesList.js
+++ src/containers/Articles/ArticlesPage/ArticlesList.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // LAYOUT
 import { List, ListItem } from 'material-ui/List';
 
