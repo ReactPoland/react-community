6cfaa6c3e8f1eff5f3051f97f8c7f81ba020e5b1
Robert Kirsz <robert.kirsz@gmail.com>
2017-04-13 15:08:22 +0200

    Redux actions for adding comments

--- src/redux/modules/conversationModule.js
+++ src/redux/modules/conversationModule.js
@@ -3,19 +3,34 @@ export const LOAD_CONVERSATION_REQUEST = 'LOAD_CONVERSATION_REQUEST';
 export const LOAD_CONVERSATION_SUCCESS = 'LOAD_CONVERSATION_SUCCESS';
 export const LOAD_CONVERSATION_FAIL = 'LOAD_CONVERSATION_FAIL';
 
+export const SUBMIT_COMMENT_REQUEST = 'SUBMIT_COMMENT_REQUEST';
+export const SUBMIT_COMMENT_SUCCESS = 'SUBMIT_COMMENT_SUCCESS';
+export const SUBMIT_COMMENT_FAIL = 'SUBMIT_COMMENT_FAIL';
+
 const initialState = {
   id: null,
   comments: [],
-  conversationLoaded: false,
+  // Loading conversation
   loadingConversation: false,
-  loadConversationError: false
+  conversationLoaded: false,
+  loadConversationError: false,
+  // Adding comment
+  addingComment: false,
+  commentAdded: false,
+  addCommentError: false
 };
 
 // --- ACTIONS ---
 export const loadConversation = (articleId) => ({
   requestName: 'Load conversation',
   types: [LOAD_CONVERSATION_REQUEST, LOAD_CONVERSATION_SUCCESS, LOAD_CONVERSATION_FAIL],
-  promise: (client) => client.post('/conversation/loadConversation', { data: { id: articleId } })
+  promise: (client) => client.post('/article/loadComments/', { data: { articleId } })
+});
+
+export const submitComment = ({ articleId, body }) => ({
+  requestName: 'Submit comment',
+  types: [SUBMIT_COMMENT_REQUEST, SUBMIT_COMMENT_SUCCESS, SUBMIT_COMMENT_FAIL],
+  promise: (client) => client.post('/self/newComment/', { data: { articleId, body } })
 });
 
 // -- REDUCER --
@@ -31,21 +46,43 @@ export default (state = initialState, action = {}) => {
         loadConversationError: false
       };
     case LOAD_CONVERSATION_SUCCESS:
-      const conversation = action.result.message;
+      const conversation = action.result.message[0] || [];
 
       return {
         ...state,
         id: conversation.id,
-        comments: conversation.comments,
+        comments: action.result.message,
         loadingConversation: false,
         conversationLoaded: true
       };
     case LOAD_CONVERSATION_FAIL:
+
       return {
         ...state,
         loadingConversation: false,
         conversationLoaded: false,
         loadConversationError: true
+      };
+    case SUBMIT_COMMENT_REQUEST:
+      return {
+        ...state,
+        addingComment: true,
+        commentAdded: false,
+        addCommentError: false
+      };
+    case SUBMIT_COMMENT_SUCCESS:
+      return {
+        ...state,
+        addingComment: false,
+        commentAdded: true,
+        addCommentError: false
+      };
+    case SUBMIT_COMMENT_FAIL:
+      return {
+        ...state,
+        addingComment: false,
+        commentAdded: false,
+        addCommentError: true
       };
     default:
       return state;
