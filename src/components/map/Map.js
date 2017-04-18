ffda26da0d90281d8eef2f5c0ba46af2e9cbddf4
Daniel Chmiel <daniel.chmiel@outlook.com>
2017-04-18 13:27:11 +0200

    switch to prop-types package

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet-universal';
 import UserMarker from './UserMarker';
 import EventMarker from './EventMarker';
