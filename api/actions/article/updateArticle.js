1dfe3c6ff4c8c4c084dc317fac9851718ab19145
Tolik <talibasya@gmail.com>
2017-05-11 14:58:54 +0200

    add link,type,description fields for articles

--- api/actions/article/updateArticle.js
+++ api/actions/article/updateArticle.js
@@ -2,7 +2,7 @@ const ArticleModel = require('../../db').articles;
 import * as ArticleValidation from '../../utils/validation/article';
 import { getSlug } from '../../utils/slug';
 
-const updateArticleRequest = async ({title, content, id, previewSize}) => {
+const updateArticleRequest = async ({title, content, id, previewSize, link, description}) => {
   const currArticle = await ArticleModel.findOne({
     where: {
       id
@@ -12,7 +12,10 @@ const updateArticleRequest = async ({title, content, id, previewSize}) => {
   const articleBody = ArticleValidation.checkArticleBody({
     title: title || currArticle.title,
     content: content || currArticle.title,
-    previewSize: previewSize || currArticle.previewSize
+    previewSize: previewSize || currArticle.previewSize,
+    link: link || currArticle.link,
+    description: description || currArticle.description,
+    type: currArticle.type
   });
 
   return await ArticleModel.update({
