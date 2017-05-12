b7c933f424e4606c0557f801eec5878be59f0552
Tolik <talibasya@gmail.com>
2017-05-12 10:55:48 +0200

    fix article updategs

--- api/actions/article/updateArticle.js
+++ api/actions/article/updateArticle.js
@@ -1,12 +1,16 @@
 const ArticleModel = require('../../db').articles;
 import * as ArticleValidation from '../../utils/validation/article';
 import { getSlug } from '../../utils/slug';
+const sequelize = require('../../db').sequelize;
 
 const updateArticleRequest = async ({title, content, id, previewSize, link, description}) => {
   const currArticle = await ArticleModel.findOne({
     where: {
       id
     }
+  }).then(item => {
+    if (!item) throw new Error('article not found');
+    return item;
   });
 
   const articleBody = ArticleValidation.checkArticleBody({
@@ -20,7 +24,7 @@ const updateArticleRequest = async ({title, content, id, previewSize, link, desc
 
   return await ArticleModel.update({
     ...articleBody,
-    slug: getSlug(articleBody.title)
+    slug: getSlug(sequelize, articleBody.title)
   }, {
     where: {
       id
