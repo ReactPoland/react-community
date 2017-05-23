ee0da23dcf24b6ab9b5c44ae484e946b9779b814
hstemplewski <huberts@reactpoland.com>
2017-05-23 12:13:56 +0200

    #22 - Mocked tutorial Page

--- src/containers/TutorialsPage/TutorialsPage.js
+++ src/containers/TutorialsPage/TutorialsPage.js
@@ -5,7 +5,13 @@ import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import { MockCard } from 'components/mocked';
+import Paper from 'material-ui/Paper';
+import ActionWork from 'material-ui/svg-icons/action/work';
+import ActionBook from 'material-ui/svg-icons/action/book';
+import ActionTrackChanges from 'material-ui/svg-icons/action/track-changes';
+import HardwarePhoneIphone from 'material-ui/svg-icons/hardware/phone-iphone';
+import SocialPublic from 'material-ui/svg-icons/social/public';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import styles from './TutorialsPage.scss';
 
 export default class TutorialsPage extends Component {
@@ -19,22 +25,40 @@ export default class TutorialsPage extends Component {
         </Jumbotron>
         <Row>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <ActionWork style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>Recommended Learning Resources</h3>
+            </Paper>
           </Col>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <ActionBook style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>Recommended Books</h3>
+            </Paper>
           </Col>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <ActionTrackChanges style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>React, Redux - the Right Way</h3>
+            </Paper>
           </Col>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <HardwarePhoneIphone style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>Migration first React Native Apps</h3>
+            </Paper>
           </Col>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <SocialPublic style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>Exploring GraphQL</h3>
+            </Paper>
           </Col>
           <Col md={6}>
-            <MockCard image />
+            <Paper zDepth={1} className={styles['TutorialsPage-card']}>
+              <ContentAdd style={{ width: '15rem', height: '15rem' }} />
+              <h3 className={styles['TutorialsPage-card-title']}>Propose your topic</h3>
+            </Paper>
           </Col>
         </Row>
       </Grid>
